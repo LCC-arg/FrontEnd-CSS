e3740f88f292776ec18f62308245994ff4061123
Gabriel <matiascoria427@gmail.com>
2023-06-27 18:26:58 -0300

    la logica que hace aparecer y desaparecer el modal esta lista

--- ViajeYa/components/loginModal/loginModal.js
+++ ViajeYa/components/loginModal/loginModal.js
@@ -1,13 +1,43 @@
-const header = document.getElementsByTagName("body");
+import { agregarEstilo } from "../../utils/agregarEstilos.js";
 
-  async function getLoginModal() {
-    const response = await fetch('/components/loginModal/loginModal.html');
-    const html = await response.text();
-    header.innerHTML = html;
-  }
+const body = document.getElementsByTagName("body")[0];
+let loginModal = null;
+
+async function getLoginModal() {
+
+  const response = await fetch('/components/loginModal/loginModal.html');
+  const html = await response.text();
+ 
+  body.innerHTML += html;
+  loginModal = document.getElementsByClassName("body__login")[0];
+
+
+  agregarEstilo("/components/header/header.css");
+
 
   //logica para hacerlo aparecer
-  let btnIngresar = document.getElementById("")
+  let btnIngresar = document.getElementsByClassName("link-right")[0];
+
+  btnIngresar.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    loginModal.style.display = "flex";
+
+  });
+
+  document.addEventListener("click", (e)=>{
+    
+    if(e.target == loginModal){
+      loginModal.style.display = "none";
+    }
+  })
+
+};
+
+
+
+
+  
   const loginComponent = {
     GetLoginModal: getLoginModal,
 };
