6209844b74e59e5f4e360ec7c770e742b00ef166
Lautaro <lautaroyanequine@gmail.com>
2023-06-28 21:18:24 -0300

    agregar componetne

---
+++ ViajeYa/components/pasaje/pasajeComponente.js
@@ -0,0 +1,57 @@
+
+export default function creacionPasaje(viaje){
+  
+    /*   
+Tiene que recibir
+Logo de la empresa
+fechaLlegada
+Fecha Salida
+Hora de llegada y de salida
+Cantidad de bolestos disponibles
+tipo de vehiculo
+terminales
+precio
+
+    
+    */
+  return ` 
+  <link rel="stylesheet" href="../components/pasaje/pasaje.css">
+
+
+  <div class="resultado-pasaje">
+      <img src="${viaje.transporte.imagen}" alt="logo Empresa">
+      <div class="fecha-salida">
+          <div class="dia-salida">
+              <h4>Sale:</h4>
+              <p>${viaje.fechaSalida}</p>
+          </div>
+          <h4 class="horario">${viaje.horarioSalida}</h4>
+          <p>Terminal Dellepiane</p>
+      </div>
+      <div class="fecha-llegada">
+          <div class="dia-llegada">
+              <h4>Llega:</h4>
+              <p>${viaje.fechaLlegada}</p>
+          </div>
+          <h4 class="horario">${viaje.horarioLlegada}</h4>
+          <p>Terminal Pinamar</p>
+      </div>
+      <p class="precio">ARS $ <span class="precio-negrita">8600</span></p>
+     <div class="tipo-vehiculo border-superior">
+      <h4 class="centrar-texto ">Bus</h4>
+     </div>
+      <div class="recorrido border-superior">
+          <h4>Recorrido</h4>
+      </div>
+      <div class="libre border-superior">
+          <h4>Libres</h4>
+          <h4 class="cantidad">12</h4>
+      </div>
+      <button class="button-reset boton-comprar border-superior">Comprar</button>
+  
+  </div>
+  
+  `;
+}
+
+
